badd7ad2f9b4a22f9958f4d4e7800ddbf31124bf
shadow <jing_hua_000@qq.com>
2019-10-26 13:46:51 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export function add (sign: any) {
+
+}
+
+export function wait (...signs: any) {
+
+}
+
+export function has (sign: any) {
+
+}
+
+export function del (sign: any) {
+
+}
+
+export function clear () {
+
+}
+
+export function list () {
+
+}
+
+export function size () {
+
+}
+
+export function namespace (namespace: string) {
+
+}
---
+++ test/demo.spec.ts
@@ -0,0 +1,12 @@
+import { eq, equals } from './shared'
+
+describe('demo', () => {
+
+  it('demo', () => {
+
+    eq(1, 1)
+    equals([1, 2, 3], [1, 2, 3])
+
+  })
+
+})
