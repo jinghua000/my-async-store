7b2f5c4ba76cd4b1866903fcb3d333a62b796f2e
jing_hua <36701496+jinghua000@users.noreply.github.com>
2019-10-27 22:59:02 +0800

    Improve build  (#2)
    Improve build

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { namespace } from './namespace';
-declare const add: (sign: any) => void, del: (sign: any) => Boolean, has: (sign: any) => Boolean, wait: (...signs: any) => Promise<void>, clear: () => void, size: () => number, store: Set<any>;
-export { add, del, has, wait, clear, size, store };
-export { namespace };
